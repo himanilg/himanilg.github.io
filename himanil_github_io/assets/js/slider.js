8bdb3f8c46b71c66b6278ebb17d624b27a07286c
Himanil Gupta <himanil.gupta@hotwaxmedia.com>
2019-05-30 00:52:52 +0530

    Committing www directory to repo

---
+++ himanil_github_io/assets/js/slider.js
@@ -0,0 +1,18 @@
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav: true,
+        items: 1,
+        animateOut: 'fadeOut'
+    })
+
+
+    $("a.owl-prev").click(function() {
+        $("div.owl-prev").click();
+    })
+    $("a.owl-next").click(function() {
+        $("div.owl-next").click();
+    })
+
+
+
+
---
+++ www/assets/js/materialize_custom.js
@@ -0,0 +1,20 @@
+// start carrousel
+   $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: false
+   });
+
+
+   // move next carousel
+   $('.moveNextCarousel').click(function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      $('.carousel').carousel('next');
+   });
+
+   // move prev carousel
+   $('.movePrevCarousel').click(function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      $('.carousel').carousel('prev');
+   });
